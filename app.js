ada633b77939383d48fa2c080f8a18b96c905f9c
Red Nguyen <rednguyen1997@gmail.com>
2025-01-13 00:27:58 -0600

    modify package json

--- app.js
+++ app.js
@@ -102,6 +102,8 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 })
 
-app.listen(5000, () => {
-    console.log('Server listening on port 5000');
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
   });
